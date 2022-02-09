460c962b8d30b7e9436a01d94e6c1f511a7a5ebe
BoleBorson <corsonca23@mail.vmi.edu>
2022-02-08 20:05:42 -0500

    added full reservation cookie functionaility and middleware

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const checkAvailable = require('./boxReservation')
 
 const express = require('express')
 var bodyParser = require('body-parser')
+var cookieParser = require('cookie-parser')
+var cors = require('cors')
 const cookie = require('cookie')
 const app = express()
 const port = 3001
 
 app.use(bodyParser.json())
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/api/reservation', (req, res) => {
+function middleware(req, res, next) {
+  console.log(req.cookies)
+  next()
+}
+
+app.post('/api/reservation', middleware, (req, res) => {
 
   console.log("Incoming")
 
     checkAvailable(req.body, (token) => {
       if(token != null) {
-        res.setHeader("Set-Cookie", cookie.serialize("token", token, {
+        res.setHeader("Set-Cookie", cookie.serialize("reservation", token, {
           httpOnly: true,
           //secure: needs to be set to https only but in dev we dont have that
           maxAge: 60 * 60,
