9dec1ea42e120226e84ed5aeffdfc50730803fd7
BoleBorson <corsonca23@mail.vmi.edu>
2022-03-01 17:13:49 -0500

    t

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 var bodyParser = require('body-parser')
 const cookie = require('cookie')
 const app = express()
-const port = 3000
+const port = 2000
 
 app.use(bodyParser.json())
 
