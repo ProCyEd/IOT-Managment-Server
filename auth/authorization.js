aaf4aa11089faa6da13b2c339850062eac5b2b9f
BoleBorson <corsonca23@mail.vmi.edu>
2022-03-03 10:13:32 -0500

    fix auth

--- auth/authorization.js
+++ auth/authorization.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers['cookie']
-    const token = authHeader && authHeader.split('=')[1]
+    const token = authHeader && authHeader.split('session=')[1]
     if(token == null) return res.send({status: "No Token", verified: false})
 
     jwt.verify(token, "secret_key", (err, data) => {
